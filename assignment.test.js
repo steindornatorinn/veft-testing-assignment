e689ed0877f06ab737a3991872dd116a1a1ca84c
Steindor Bragason <steinibragason@gmail.com>
2017-10-24 23:42:47 +0000

    lokaskil

--- assignment.test.js
+++ assignment.test.js
@@ -2,9 +2,31 @@
 import {add, throws} from './index';
 import errorFunction from './errorFunction';
 import * as indexModule from './index';
+import mongo from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import app from './app'
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 jest.mock('./errorFunction');
 errorFunction.mockImplementation(() => 'errorFunction');
+
+let mongoServer;
+let server;
+
+mongoose.Promise = global.Promise;
+
+beforeAll(() => {
+  mongoServer = new mongo();
+  mongoServer.getConnectionString().then(mongoUri =>{
+    mongoose
+      .connect(mongoUri, {
+         useMongoClient: true,
+    }).then(db => {
+      server = app(db);
+      resolve();
+    });
+  });
+});
 
 describe('#### 1', () => {
   test('1+1 = 2', () => {
