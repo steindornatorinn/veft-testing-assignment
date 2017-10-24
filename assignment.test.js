bb1dee698051841a6ceed7184abf69f4bde715ed
Steindor Bragason <steinibragason@gmail.com>
2017-10-24 21:06:12 +0000

    finished first part of the assignment

--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,13 @@
-describe('Remove me', () => {
-  test('Remove this', () => {
-    expect(1).toBe(1);
+import {add} from './index';
+
+describe('Add tests', () => {
+  test('1+1 = 2', () => {
+    expect(add(1, 1)).toBe(2);
+  });
+  test('adding null and a number works', () => {
+    expect(add(null, 1)).toBe(1);
+  });
+  test('adding nothing returns NaN', () => {
+    expect(add()).toEqual(NaN);
   });
 });
