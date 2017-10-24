01689a664cdb73d1ae899eddf42f5293cc0ee20d
Steindor Bragason <steinibragason@gmail.com>
2017-10-24 21:54:24 +0000

    part 3

--- assignment.test.js
+++ assignment.test.js
@@ -1,13 +1,24 @@
-import {add} from './index';
+import * as indexModule from './index';
+//import {loop, add} from './index'
 
 describe('Add tests', () => {
   test('1+1 = 2', () => {
-    expect(add(1, 1)).toBe(2);
+    expect(indexModule.add(1, 1)).toBe(2);
   });
   test('adding null and a number works', () => {
-    expect(add(null, 1)).toBe(1);
+    expect(indexModule.add(null, 1)).toBe(1);
   });
   test('adding nothing returns NaN', () => {
-    expect(add()).toEqual(NaN);
+    expect(indexModule.add()).toEqual(NaN);
   });
+});
+
+
+describe('Loop tests', () => {
+  const addSpy = jest.spyOn(indexModule, 'add');
+  test('calls add 3 times if looping 4 times', () => {
+    indexModule.loop(4);
+    expect(addSpy).toHaveBeenCalledTimes(3);
+  });
+
 });
